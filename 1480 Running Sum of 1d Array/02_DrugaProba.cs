9aeb8d1b47ce23e6df1df53c45998f4dcb93bcf0
Sebastian <sebsier@gmail.com>
2022-10-25 00:31:07 +0100

    WIP running sum podejscie drugie

--- 1480 Running Sum of 1d Array/02_DrugaProba.cs
+++ 1480 Running Sum of 1d Array/02_DrugaProba.cs
@@ -8,5 +8,13 @@ namespace ZadaniaProgramistyczne._1480_Running_Sum_of_1d_Array
 {
     public class DrugaProba
     {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i - 1] + nums[i];
+            }
+            return nums;
+        }
     }
 }
