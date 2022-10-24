b0eddd1b13912b776a89fbea7fc2576a244ca2cd
Sebastian <sebsier@gmail.com>
2022-10-25 00:28:13 +0100

    WIP pierwsze zadanie i okablowanie

---
+++ Okablowanie/ITestable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaniaProgramistyczne.Okablowanie
+{
+    public interface ITestable<T1>
+    {
+        string Test(T1 input);
+        T1 WygenerujDaneWejsciowe();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+//1480.Running Sum of 1d Array
+while (true)
+{
+    int[] raz = new int[] { 1, 2, 3, 4, 5 };
+
+    var result = ZadaniaProgramistyczne._1480_Running_Sum_of_1d_Array.PierwszaProba.RunningSum(raz);
+
+    foreach(int i in result) Console.Write(i + " ");
+    Console.WriteLine();
+
+    Console.ReadLine();
+}
